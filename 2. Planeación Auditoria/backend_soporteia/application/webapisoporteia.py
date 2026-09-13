bb11c5a383a4bcfc3667d9a00eabdca09581709b
Andrés Agudelo <andres.agudelo996@pascualbravo.edu.co>
2026-09-13 01:17:09 -0500

    actualizacion api

---
+++ 2. Planeación Auditoria/backend_soporteia/application/webapisoporteia.py
@@ -0,0 +1,10 @@
+
+# webapisoporteia
+
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="API Soporte IA",
+    description="API para la pagina de Soporte IA",
+    version="1.0.0"
+)
---
+++ 2. Planeación Auditoria/backend_soporteia/main.py
@@ -0,0 +1,17 @@
+
+# main
+
+import uvicorn
+
+def start():
+    print("Starting server...")
+    uvicorn.run(
+                "application.webapisoporteia:app",
+                host="127.0.0.1",
+                port=7000,
+                reload=True
+                )
+    print("Server is running.")
+
+if __name__ == "__main__":
+    start()
